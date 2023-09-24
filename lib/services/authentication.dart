a5bdd4259bdd828c86bf1c4212bde5603bd0018f
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-09-24 20:20:42 +0300

    removed specific code

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -2,8 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 enum AuthStatus {
   success,
-  invalidPhone,
-  timeOut,
 }
 
 class Authentication {
@@ -12,35 +10,4 @@ class Authentication {
 
   static FirebaseAuth get auth => _auth;
   static AuthStatus? status;
-
-  String? verificationId;
-
-  static authWithPhone({String? phone, String? sms}) async {
-    return _auth.verifyPhoneNumber(
-      phoneNumber: phone,
-      verificationCompleted: (PhoneAuthCredential credential) async {
-        // ANDROID ONLY!
-        await _auth.signInWithCredential(credential);
-      },
-      verificationFailed: (FirebaseAuthException e) {
-        if (e.code == 'invalid-phone-number') {
-          status = AuthStatus.invalidPhone;
-          return;
-        }
-      },
-      codeSent: (String verificationId, int? resendToken) async {
-        if (sms != null) {
-          PhoneAuthCredential credential = PhoneAuthProvider.credential(
-            verificationId: verificationId,
-            smsCode: sms,
-          );
-          // Sign the user in (or link) with the credential
-          await _auth.signInWithCredential(credential);
-        }
-      },
-      codeAutoRetrievalTimeout: (String verificationId) {
-        status = AuthStatus.timeOut;
-      },
-    );
-  }
 }
